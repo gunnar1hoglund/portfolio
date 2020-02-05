1634aef11772a838f4cfcb7f10a4181efc08f44a
gunnar <gunnar1hoglund@gmail.com>
2020-02-05 13:18:17 -0800

    got rid of some stuff

--- script.js
+++ script.js
@@ -1,12 +1,25 @@
 
 var geoff1 = document.getElementById("geoff1")
 var geoff2 = document.getElementById("geoff2")
+var geoff3 = document.getElementById("geoff3")
+var geoff4 = document.getElementById("geoff4")
 
 // generating random numbers to decide what the main page will display
-if(Math.floor((Math.random() * 1000) + 1) == 7) {
+
+// switch statment
+
+switch(Math.floor((Math.random() * 4) + 1)) {
+    case 1: 
     geoff1.style.display = ''
-}
-else {
+        break;
+    case 2: 
     geoff2.style.display = ''
+        break; 
+    case 3: 
+    geoff3.style.display = ''
+        break;
+    case 4: 
+    geoff4.style.display = ''
+        break;        
 }
 
