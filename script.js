c030ce8ff6bac30cc0fe4c0953c23bc88d67559f
gunnar <gunnar1hoglund@gmail.com>
2021-02-11 16:48:53 -0800

    removed outdated resume

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
+/*
+    TODO:   
+        - add sass preprocessing to styling
+        - randomly generate hex color
+        - add button that changes the color of the website
+        - button that changes the theme of the website
+        - make random color button respect the theme button and choose
+          colors with darker hues
 
+
+*/
 var geoff1 = document.getElementById("geoff1")
 var geoff2 = document.getElementById("geoff2")
 var geoff3 = document.getElementById("geoff3")
@@ -23,3 +33,7 @@ switch(Math.floor((Math.random() * 4) + 1)) {
         break;        
 }
 
+let white = 0xFFFFFF
+// complementary = 0xFFFFFF - (yourColor) where yourColor is a 
+// varible containing a hex color value
+complementary(randColor) = () => white - randColor
